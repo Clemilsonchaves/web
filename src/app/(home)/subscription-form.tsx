6efa3fdef1d42effd4b311845563ea0bb6c4283c
clemilson chaves <cle_milson_silva@hotmail.com>
2025-03-10 04:53:24 -0300

    Add referrer tracking to subscription form and update stats component to display subscriber-specific data

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -4,6 +4,8 @@ import { InputField, InputIcon, InputRoot } from '@/components/input'
 import { subscribeToEvent } from '@/http/api'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight, Mail, User } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -15,6 +17,9 @@ const subscriptionSchema = z.object({
 type SubscriptionSchema = z.infer<typeof subscriptionSchema>
 
 export function SubscriptionForm() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
   const {
     register,
     handleSubmit,
@@ -24,7 +29,9 @@ export function SubscriptionForm() {
   })
 
   async function onSubscribe({ name, email }: SubscriptionSchema) {
-    const { subscriberId } = await subscribeToEvent({ name, email })
+    const referrer = searchParams.get('referrer')
+    const { subscriberId } = await subscribeToEvent({ name, email, referrer })
+    router.push(`/invite/${subscriberId}`)
   }
   return (
     <form
