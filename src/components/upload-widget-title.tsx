404774949813bff491bf9ed3a0466e96aae26867
clemilsonchaves <clemilsonchaves.1992@gmail.com>
2026-02-28 06:58:29 -0300

    fix: update upload status display to reflect actual uploads count in UploadWidgetUploadList component

--- src/components/upload-widget-title.tsx
+++ src/components/upload-widget-title.tsx
@@ -1,18 +1,16 @@
 import { UploadCloud } from "lucide-react";
+import { useUploads } from "../store/uploads";
 
-export function UploadWidgetTitle() {   
-   const isThereAnyPendingUpload = true;  
-   const uploadGlobalpercentage = 66;
+export function UploadWidgetTitle() {
+  const { uploads } = useUploads();
+  const isThereAnyPendingUpload = uploads.size > 0;
 
     return (
 
         <div className="flex items-center gap-1.5 font-medium">
          <UploadCloud strokeWidth={1.5} className="size-4 text-zinc-400" />
          {isThereAnyPendingUpload ? (
-            <span className="flex items-center gap-1">
-              Uploads
-              <span className="text-xs text-zinc-400 tabular-nums">{uploadGlobalpercentage}%</span>
-            </span>
+        <span className="flex items-center gap-1">Uploads</span>
          ) : (
          <span className="text-sm font-medium">Faça upload dos seus arquivos</span>
          )}
